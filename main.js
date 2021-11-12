b1b817cccf33798466cf54651e4f542c4e0958f9
saioufe <saioufeyoutube@yahoo.com>
2021-11-12 13:36:46 +0300

    main

--- main.js
+++ main.js
@@ -9,8 +9,7 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
 });
-
-
+//
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
